309766775d57d557ece32a1572db444fa2075517
abstract⁷ <citrixlabph@gmail.com>
2026-03-25 13:22:13 +0800

    Update Module1Page.jsx

--- frontend/src/pages/Module1Page.jsx
+++ frontend/src/pages/Module1Page.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import { brand, so1Content, saveFormLocally } from "../mockData";
+import { brand, so1Content, saveFormLocally } from "../mock";
 import { Card, CardContent, CardHeader, CardTitle } from "../components/ui/card";
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "../components/ui/accordion";
 import { Button } from "../components/ui/button";
